934ba823cdde909c0ae701cf3b21c759ef323a42
Mykhailo Oshurko <mykhailo.oshurko.up@gmail.com>
2025-12-22 10:19:47 +0100

    Dodaj pliki projektów.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using System.Diagnostics;
+using BDwAI_BugTrackSys.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BDwAI_BugTrackSys.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
